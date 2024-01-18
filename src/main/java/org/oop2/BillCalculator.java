6d57e69dc1a407ef0ff889ddc7630079948bc7c3
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-01-18 23:19:40 +0100

    Task-6--commit_01

---
+++ src/main/java/org/oop2/BillCalculator.java
@@ -0,0 +1,17 @@
+package org.oop2;
+
+public class BillCalculator {
+
+    static double calculate(double billValue, float servicePayment) {
+        return billValue + servicePayment;
+    }
+
+    static double calculate(double billValue, float servicePayment, double percDiscount) {
+        return billValue - (billValue * percDiscount / 100) + servicePayment;
+    }
+
+    static double calculate (double billValue, float servicePayment, short packagingFee){
+        return billValue + servicePayment + packagingFee;
+    }
+
+}
---
+++ src/main/java/org/oop2/Main.java
@@ -0,0 +1,14 @@
+package org.oop2;
+
+public class Main {
+    public static void main(String[] args) {
+        double [] bill = new double[3];
+        bill[0] = BillCalculator.calculate(120d, 5.50f);
+        bill[1] = BillCalculator.calculate(120d, 5.50f, 15f);
+        bill[2] = BillCalculator.calculate(120d, 5.50f, (short) 3);
+
+        for (Double bills: bill) {
+            System.out.println(bills);
+        }
+    }
+}
